a332afa28b3b6148dd0e6ca91c62158f54995daf
Vivek Modh <vivekmodh03@gmail.com>
2025-07-14 11:58:37 +0530

    added python for devops learning

--- backup.py
+++ backup.py
@@ -2,13 +2,13 @@ import shutil
 import datetime
 import os
 
-def  backup_files(source_dir, backup_dir):
+def backup_files(source_dir, backup_dir):
   today = datetime.datetime.today()
   backup_file_name = os.path.join(backup_dir, f"backup_{today}tag.gz")
   shutil.make_archive(backup_file_name.replace("tar.gz",''),'gztar',source_dir)
 
-source_directory = input("Enter the source directory to backup: ")
-backup_directory = input("Enter the backup directory: ")      
-backup_files(source_directory, backup_directory)
+source_directory = "Enter the source directory to backup: "
+backup_directory = "Enter the backup directory: "
+backup_files(source_dir, backup_dir)
 
 
